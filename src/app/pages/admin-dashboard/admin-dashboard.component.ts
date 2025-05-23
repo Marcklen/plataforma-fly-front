5d1c292835c6e52818fa5ac48fe282c2406f5b00
marcklen <marcklen@icloud.com>
2025-05-23 16:50:54 -0300

    fix: ajustado home e iniciado dashboard de admin

--- src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit{
+  totalUsuariosAdmin: number = 0;
+  totalUsuariosComuns: number = 0;
+  emailsEnviados: number = 0;
+  emailsPendentes: number = 0;
 
+   constructor() {}
+
+  ngOnInit(): void {
+    this.carregarEstatisticas();
+  }
+
+  carregarEstatisticas() {
+    // Simulação de carregamento de dados
+    this.totalUsuariosAdmin = 3;
+    this.totalUsuariosComuns = 15;
+    this.emailsEnviados = 60;
+    this.emailsPendentes = 10;
+  }
 }
